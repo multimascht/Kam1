802ded8d28804254233445c2fa8a5b243cc7afb6
M G <multimascht@gmail.com>
2023-02-09 11:16:57 +0100

    zadanie dzień 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadanie domowe dzień 4
+
+string name = "Ewa";
+int age = 33;
+bool woman = true;
+
+if (age == 33 && name == "Ewa" && woman == true)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 30 && !(name == "Ewa") && woman == true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age < 18 && !(name == "Ewa") && woman == false) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+      
